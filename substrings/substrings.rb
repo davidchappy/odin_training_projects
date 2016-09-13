6e466e3cdcc3ffbfeb2403dadd9b3b48a20c7666
David Chapman <davidchappy@gmail.com>
2016-09-13 11:04:05 -0300

    Complete Substring

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,3 +1,15 @@
-def substrings
-  
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, dictionary)
+  results = {}
+  word.downcase!
+  dictionary.each do |entry|
+    if word.include?(entry)
+      sub_count = word.scan(/(?=#{entry})/).count
+      results[entry] = sub_count
+    end
+  end
+  results
 end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
