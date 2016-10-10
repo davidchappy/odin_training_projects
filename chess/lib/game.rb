180743c9b036af5ef72c5b13d35093e9aac647f6
David Chapman <davidchappy@gmail.com>
2016-10-10 15:50:31 -0300

    Add process_turns method for flow of game and default player names

--- chess/lib/game.rb
+++ chess/lib/game.rb
@@ -12,11 +12,22 @@ class Game
   end
 
   def initialize
-    @player1 = Player.new(1, "white")
-    @player2 = Player.new(2, "blue")
+    @player1 = Player.new(1, "white", "David")
+    @player2 = Player.new(2, "blue", "Kristin")
     @current_player = set_starting_player
-
     puts @current_player.name
   end
+
+  # def process_turns
+  #   until game_over?
+  #     @current_player = switch_players(@current_player)
+  #     GameIO.print_board(@board.board)
+  #     GameIO.print_turn_update(@current_player)
+  #     move = @current_player.move
+  #     @board.board = process_move(move)
+  #   end
+  #   GameIO.print_board(@board.board)
+  #   start_again
+  # end
 
 end
--- chess/lib/player.rb
+++ chess/lib/player.rb
@@ -3,8 +3,8 @@ class Player
 
   attr_reader :name
 
-  def initialize(num, color)
-    @name = GameIO.request_player_name(num, color)
+  def initialize(num, color, name=false)
+    @name = name || GameIO.request_player_name(num, color)
   end
 
 end
