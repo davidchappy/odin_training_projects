33ed36bf74293155aed4a84a9d818556a2ecb6c4
David Chapman <davidchappy@gmail.com>
2016-10-10 15:40:47 -0300

    Initialize Game with Player1 and Player2

--- chess/lib/game.rb
+++ chess/lib/game.rb
@@ -1,10 +1,22 @@
 # -*- encoding : utf-8 -*-
 class Game
+  include ChessHelpers
+
+  attr_reader :board, :player1, :player2
+  attr_accessor :current_player
 
   def self.start
     @game = Game.new
     @board = Board.new
     puts @board.board
+  end
+
+  def initialize
+    @player1 = Player.new(1, "white")
+    @player2 = Player.new(2, "blue")
+    @current_player = set_starting_player
+
+    puts @current_player.name
   end
 
 end
--- chess/lib/player.rb
+++ chess/lib/player.rb
@@ -1,1 +1,10 @@
 # -*- encoding : utf-8 -*-
+class Player
+
+  attr_reader :name
+
+  def initialize(num, color)
+    @name = GameIO.request_player_name(num, color)
+  end
+
+end
