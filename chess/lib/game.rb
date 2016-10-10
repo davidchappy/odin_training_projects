ef478ce320656c5fbe7d3234c854de3e2df1c908
David Chapman <davidchappy@gmail.com>
2016-10-10 15:21:12 -0300

    Create remaining files for chess project

--- chess/lib/game.rb
+++ chess/lib/game.rb
@@ -1,7 +1,10 @@
+# -*- encoding : utf-8 -*-
 class Game
 
   def self.start
-    
+    @game = Game.new
+    @board = Board.new
+    puts @board.board
   end
 
 end
--- chess/spec/game_spec.rb
+++ chess/spec/game_spec.rb
@@ -1,13 +1,10 @@
+# -*- encoding : utf-8 -*-
 require './chess.rb'
 
 describe Game do
 
   let(:game) { Game.new }
 
-  before(:each) do
-    allow(game).to receive(:puts)
-    allow(game).to receive(:gets)
-  end
 
   
 
