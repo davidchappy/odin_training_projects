942f6372ec72d4e836fab21a1789b3b207a70870
David Chapman <davidchappy@gmail.com>
2016-10-27 11:16:53 -0300

    Add Seed data

--- members-only/db/seeds.rb
+++ members-only/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def user(name, email, password)
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+    )
+end
+
+def post(title, content, user)
+  Post.create!(
+    title: title,
+    content: content,
+    user_id: user
+    ) 
+end
+
+
+user("Example User", "members@example.com", "members_only")
+user("Fred Mertz", "fred@ilovelucy.com", "password")
+user("Ethel Mertz", "ethel@ilovelucy.com", "password")
+
+post("My first post", "This is the content of my first post", 1)
+post("What I heard last night", "You'll never believe it, but last night when I was walking by Lucy's apartment door...
+", 3)
+post("I can't believe I have to...", "Fix their leaky faucet again. Grumble, grumble.", 2)
