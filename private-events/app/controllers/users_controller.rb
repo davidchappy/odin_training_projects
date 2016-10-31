713f73d0f9f79586dd5dcffc508bf8580dda3276
David Chapman <davidchappy@gmail.com>
2016-10-31 09:53:44 -0300

    Create and set up basic user sign up and show functionality

---
+++ private-events/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User created."
+      redirect_to @user
+    else
+      flash.now[:danger] = "Error with form. Please sign up again."
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
