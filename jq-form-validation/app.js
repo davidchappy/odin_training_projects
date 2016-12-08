35a7a5c65e4f5e0ea4c02cd88c21ec8f779cde44
David Chapman <davidchappy@gmail.com>
2016-12-08 17:05:06 -0300

    Functional near-replica of Mint.com signup form

--- jq-form-validation/app.js
+++ jq-form-validation/app.js
@@ -4,13 +4,14 @@ var $form = $('#mintSignupForm');
 var fields = {
   email: {
     tag: $('#mintEmail'),
-    requiredMessage: "Please enter email address"
+    tooltip: "You'll use this as your User ID"
   },
   emailConfirm: {
     tag: $('#mintEmailConfirm'),
   },
   password: {
     tag: $('#mintPassword'),
+    tooltip: "Create a password you've never used before. This will help keep your account safe."
   },
   passwordConfirmation: {
     tag: $('#mintPasswordConfirm'),
@@ -19,8 +20,7 @@ var fields = {
     tag: $('#mintCountry'),
   },
   zip: {
-    tag: $('#mintZip'),
-    requiredMessage: "Please enter a valid zip code"
+    tag: $('#mintZip')  
   },
   remember: {
     tag: $('#mintRemember'),
@@ -49,7 +49,7 @@ function hasCase(value) {
       hasLowerCase = true;
     }
   }
-  if(value && hasLowerCase && hasUpperCase) {
+  if(hasLowerCase && hasUpperCase) {
     return true;
   } else {
     return false;
@@ -68,17 +68,25 @@ function hasNumber(value) {
 function hasSymbol(value) {
   var symbols = ["(", ")", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "=", "|", "\\", "{", "}", "[", "]", ":", ";", "<", ">", ".", "?", "/"];
   for(var i=0; i<value.length; i++) {
-    if(symbols.contains(value[i])) {
+    if(symbols.includes(value[i])) {
       return true;
     };
   }
   return false;
 }
 
+// http://stackoverflow.com/questions/160550/zip-code-us-postal-code-validation
+function isValidZip(zip) {
+  return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zip);
+}
+
 
 // Main functions
-function validate(element) {
+function validate(element, finalKeypress) {
+  element.tag.siblings('.errors').empty();
   element.errors = [];
+  var errors = element.errors;
+
   // Remove Browser's default validation message;
   element.validationMessage = null;
   
@@ -87,48 +95,58 @@ function validate(element) {
 
   // Validate presence
   if(htmlTag.required && htmlTag.value === '' && element.requiredMessage) {
-    element.errors.push(element.requiredMessage);
+    errors.push(element.requiredMessage);
   } 
 
   if(htmlTag.type === 'email') {
     if(htmlTag.id === 'mintEmailConfirm' 
       && htmlTag.value != fields.email.tag[0].value) {
-      element.errors.push('Confirm email field does not match the email address field.')
-    } else if (htmlTag.id === 'mintEmail' && !validEmail(htmlTag.value)) {
-      element.errors.push('Please enter a valid email address');
+      errors.push('Confirm email field does not match the email address field.')
+    } 
+    else if (htmlTag.id === 'mintEmail' && !validEmail(htmlTag.value)) {
+      var message = 'Please enter a valid email address';
+      errors.push(message);
     }
   } else if(htmlTag.type === 'password') {
     if(htmlTag.id === 'mintPassword') {
       if(htmlTag.value.length < 8) {
-        element.errors.push('Use 8 or more characters');
+        errors.push('Use 8 or more characters');
       }
       if(!hasNumber(htmlTag.value)) {
-        element.errors.push('Use upper and lower case letters (e.g. Aa)');
+        errors.push('Use upper and lower case letters (e.g. Aa)');
       }
       if(!hasCase(htmlTag.value)) {
-        element.errors.push('Use a number (e.g. 1234)');
+        errors.push('Use a number (e.g. 1234)');
       }
       if(!hasSymbol(htmlTag.value)) {
-        element.errors.push('Use a symbol (e.g. !@#$)');
+        errors.push('Use a symbol (e.g. !@#$)');
       }
     } else if(htmlTag.id === 'mintPasswordConfirm' 
       && htmlTag.value != fields.password.tag[0].value) {
-      element.errors.push('Confirm password field does not match the password field.')
+      console.log(htmlTag.value);
+      console.log(fields.password.tag[0].value);
+      errors.push('Confirm password field does not match the password field.')
+    } 
+  } 
+
+  if(element === fields.zip) {
+    if(htmlTag.value.length < 5 || !isValidZip(htmlTag.value)) {
+      errors.push('Please enter a valid zip code.');
     }
-  } else {
-    console.log(htmlTag);
   }
 
-  var errors = element.errors;
   if(errors.length) {
     element.tag.addClass('has-errors');
-    var list = element.tag.next('ul');
+    var list = element.tag.siblings('ul');
     if(errors.length > 1) list.addClass('multiple');
     $.each(errors, function(index, val) {
       var li = $('<li>' + val + '</li>');
       list.append(li);
     });
     list.show();
+  } else {
+    element.tag.removeClass('has-errors');
+    element.tag.addClass('valid-field');
   }
 }
 
@@ -141,8 +159,52 @@ function setup() {
 function listen() {
   console.log('listening');
 
-  // tooltips
-  
+  $.each(fields, function(index, val) {
+    // tooltips
+    if(val.tooltip) {
+      val.tag.on('focus', function(event) {
+        toggleToolTip(val.tag, val.tooltip);
+      }).on('blur', function(event) {
+        toggleToolTip(val.tag);
+      })
+    }
+  });
+
+  fields.password.tag.on('focus', function(event) {
+    fields.password.tag.addClass('insecure');
+    // fields.password.tag.siblings('.errors').addClass('errors-selected');
+    validate(fields.password);
+  });
+  // fields.password.tag.on('blur' function(event) {
+  //   fields.password.tag.removeClass('password-selected');
+  //   fields.password.tag.siblings('.errors').removeClass('errors-selected');
+  // });
+
+  $.each(fields, function(index, val) {
+    // keypresses
+    val.tag.on('keypress', function(event) {
+      console.log("typed");
+      val.typed = true;  
+    });
+    val.tag.on('blur', function(event) {
+      if(val.typed) {
+        validate(val);
+      }
+    });
+  });
+
+  $.each(fields, function(index, val) {
+
+  });
+}
+
+function toggleToolTip(field, message) {
+  var parent = field.parent();
+  if(field.siblings('.tooltip').length) {
+    parent.find('.tooltip').remove();
+  } else {
+    field.after('<span class="tooltip">' + message + '</span>')
+  }
 }
 
 // testing only
