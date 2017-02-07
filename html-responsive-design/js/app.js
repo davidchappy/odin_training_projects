4e5d0988dd956d83939dc7fa501e1d0a5b042b9c
David Chapman <davidchappy@gmail.com>
2017-02-07 13:46:34 -0300

    Complete navigation and hero sections

--- html-responsive-design/js/app.js
+++ html-responsive-design/js/app.js
@@ -1,8 +1,13 @@
 jQuery(document).ready(function($) {
-  $('.navigation').on('click', '#nav-trigger', function(event) {
-    $('.mob-nav').css('display', 'block');
+  $('header').on('click', '#nav-trigger', function(event) {
+    $('.mob-nav').show();
   });
   $('header').on('click', '#close-nav', function(event) {
-    $('.mob-nav').css('display', 'none');
+    $('.mob-nav').hide();
+  });
+  $(window).resize(function() {
+    if($(window).width() >= 768) {
+      $('.mob-nav').hide();
+    };
   });
 });
